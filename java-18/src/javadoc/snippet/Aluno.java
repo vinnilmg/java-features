18ff785da5aed2a52403d421fbdc3a1bbbd1d400
Vinicius Gomes <vini.delimagomes@gmail.com>
2026-02-24 20:41:36 -0300

    java 18 feature

---
+++ java-18/src/javadoc/snippet/Aluno.java
@@ -0,0 +1,36 @@
+package javadoc.snippet;
+
+public class Aluno {
+    private String nome;
+    private String matricula;
+
+    /**
+    Construtor da classe Aluno.
+    {@snippet :
+        public Aluno(String nome, String matricula) {
+            this.nome = nome;
+            this.matricula = matricula;
+        }
+     }
+     */
+    public Aluno(String nome, String matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
